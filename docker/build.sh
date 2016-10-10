a1216832bfec969dfe74e25b47eb72d16d92a591
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2016-10-10 23:33:51 +0200

    added builder to docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,46 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 parent=`dirname $PWD`
 #echo $parent
 
-function build_torsten {
+build_torsten() {
     docker run --rm -v "$parent":/go/src/github.com/kildevaeld/torsten \
     -w /go/src/github.com/kildevaeld/torsten/torsten \
     -ti \
     --name torsten-builder \
-    kildevaeld/go-builder sh -c make
+    torsten-builder sh -c make
 
     mv ../torsten/torsten torsten
     docker build --tag kildevaeld/torsten .
 }
 
-function build_keyval {
-    BASE=${GOPATH}/src/github.com/kildevaeld/filestore
-    str="run --rm -v $BASE:/go/src/github.com/kildevaeld/filestore \
-    -w /go/src/github.com/kildevaeld/filestore/keyval \
-    -ti \
-    --name keyval-builder \
-    kildevaeld/go-builder sh -c make"
-
-    docker $str 
-    mv ${BASE}/keyval/keyval keyval
-    #docker build --tag kildevaeld/torsten .
-}
 
 
-function build () {
+build() {
+    
     cd builder
     docker build --tag torsten-builder .
     cd ..
     
-    BASE=${GOPATH}/src/github.com/kildevaeld/filestore
-    docker run --rm -v "$parent":/go/src/github.com/kildevaeld/torsten \
-    -v "$BASE":/go/src/github.com/kildevaeld/filestore \
-    -ti \
-    --name torsten-builder torsten-builder
-
+    build_torsten
+    
     docker rmi torsten-builder
 }
 
-build_torsten
 
-docker build --tag kildevaeld/torsten .
+build
+
